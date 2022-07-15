9f73038ebbc225f9256173a1cab185b323a2bcb2
José Rocha Martins <1800250+jrochamartins@users.noreply.github.com>
2022-07-15 23:36:17 +0000

    Desafio 6: Foi necessários rodar npm update --save

--- myApp.js
+++ myApp.js
@@ -5,14 +5,17 @@ console.log('Hello World');
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", function(req, res){
-  // res.send("Hello Express");
-    absolutePath = __dirname + "/views/index.html";
-    res.sendFile(absolutePath);
-});
+//app.get("/", (req, res) => res.send("Hello Express"))
+
+app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
 
 app.get("/json", (req, res) => {
-  res.json({"message": "Hello json"});  
+  var response = "Hello json";
+  
+  if (process.env.MESSAGE_STYLE == "uppercase")
+     response = response.toUpperCase();
+    
+  res.json({"message": response});
 });
 
 
