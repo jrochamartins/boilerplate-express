9f2a5fa2c3138f2258dbac5826b625e54eb1e399
José Rocha Martins <1800250+jrochamartins@users.noreply.github.com>
2022-07-15 02:48:16 +0000

    Desafio 4: Servir pasta estática em path específico.

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ console.log('Hello World');
 app.get("/", function(req, res){
   // res.send("Hello Express");
   absolutePath = __dirname + "/views/index.html";
-  res.send
+  res.sendFile(absolutePath);
 });
 
 
