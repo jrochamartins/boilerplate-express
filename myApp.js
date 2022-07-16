52f50e9cb85e9199fc40d403286e72c2ceaf9a5b
José Rocha Martins <1800250+jrochamartins@users.noreply.github.com>
2022-07-16 00:24:35 +0000

    Desafio 7: Criando midleware.

--- myApp.js
+++ myApp.js
@@ -5,6 +5,11 @@ console.log('Hello World');
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+  console.log(req.method + " " + req.path +" - " + req.ip);
+  next();
+});
+
 //app.get("/", (req, res) => res.send("Hello Express"))
 
 app.get("/", (req, res) => res.sendFile(__dirname + "/views/index.html"));
